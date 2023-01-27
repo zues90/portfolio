602064150ac650002ee8528268fbdd2ecc0332cf
zues90 <msshekhawat66@gmail.com>
2023-01-27 16:17:18 +0530

    done changes in css

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const hamBurger = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
-
-
-function toggleButton() {
-    navList.classList.toggle('show')
-}
-
-hamBurger.addEventListener('click', toggleButton)
